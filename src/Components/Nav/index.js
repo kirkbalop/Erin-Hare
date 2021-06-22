21738d59188f43534cd99f7fab1db59695da09ed
Kirk <kirkbalop@gmail.com>
2021-06-22 09:31:59 -0700

    adjust Navbar, include logo

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -1,23 +1,21 @@
 import React from "react";
+import { Navbar, Form } from "react-bootstrap";
 
 function Nav(props) {
-  const { currentTab, setCurrentTab } = props;
   return (
-    <nav>
-      <ul className="flex-row mobile-view">
-        <li className={currentTab === "home" ? "mx-2 navActive" : "mx-2"}>
-          <span onClick={() => setCurrentTab("home")}>Home</span>
-        </li>
-        <li className={currentTab === "services" ? "mx-2 navActive" : "mx-2"}>
-          <span onClick={() => setCurrentTab("services")}>Services</span>
-        </li>
-        <li className={currentTab === "product" ? "mx-2 navActive" : "mx-2"}>
-          <span onClick={() => setCurrentTab("product")}>Product</span>
-        </li>
-        <li className={currentTab === "contact" ? "mx-2 navActive" : "mx-2"}>
-          <span onClick={() => setCurrentTab("contact")}>Contact</span>
-        </li>
-      </ul>
-    </nav>
+    <Navbar bg="light">
+      <Navbar.Brand href="#home" id="logoNav">
+        <img
+          src={process.env.PUBLIC_URL + `/assets/images/EH-logo.png`}
+          width="120"
+          height="120"
+          className="d-inline-block align-top"
+          alt="Erin Hare logo"
+        />{" "}
+        <span id="logoNavText">Erin Hare</span>
+      </Navbar.Brand>
+    </Navbar>
   );
 }
+
+export default Nav;
