a4d1c3c62df4096b838b06bde92431db77810a5a
Kirk <kirkbalop@gmail.com>
2021-07-22 15:23:32 -0700

    added to navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
+import React from "react";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 import NaviBar from "./Components/Nav";
+import ApptForm from "./Components/ApptForm";
 
 function App() {
   return (
     <>
       <NaviBar></NaviBar>
       <ApptForm />
-      <LoginForm />
     </>
   );
 }
